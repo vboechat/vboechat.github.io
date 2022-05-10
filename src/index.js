89bf42e328df17993f2d2bd6d1b0e533cd51727b
Nawiab <victor2005contato@gmail.com>
2022-05-09 22:08:54 -0300

    General changes and improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
+import './assets/styles/global.css';
 import App from './pages/App';
-import reportWebVitals from './routes/reportWebVitals';
+import reportWebVitals from './services/test/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
